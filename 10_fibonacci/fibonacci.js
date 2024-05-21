a87f5f332b098e4e4284890a8cf06530d25e6e19
Ersin Çöl <ersincol235@gmail.com>
2024-05-21 14:41:44 +0300

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function (memberIndex) {
+  if (typeof memberIndex === "string") memberIndex = parseInt(memberIndex);
+  if (memberIndex === 0) return 0;
+  if (memberIndex < 0) return "OOPS";
 
+  let arr = [];
+  for (let index = 0; index < memberIndex; index++) {
+    if (index === 0 || index === 1) {
+      arr[index] = 1;
+    } else {
+      arr[index] = arr[index - 1] + arr[index - 2];
+    }
+  }
+  return arr[memberIndex - 1];
 };
 
 // Do not edit below this line
