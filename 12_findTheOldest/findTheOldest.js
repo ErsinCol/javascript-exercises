0911c36941a3dda2de3c700d02ab197b74b65e06
Ersin Çöl <ersincol235@gmail.com>
2024-05-21 22:07:37 +0300

    Complete 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  people.sort((a, b) => {
+    const aAge = a.yearOfDeath
+      ? a.yearOfDeath - a.yearOfBirth
+      : new Date().getFullYear() - a.yearOfBirth;
+    const bAge = b.yearOfDeath
+      ? b.yearOfDeath - b.yearOfBirth
+      : new Date().getFullYear() - b.yearOfBirth;
+    return bAge - aAge;
+  });
 
+  return people[0];
 };
 
 // Do not edit below this line
